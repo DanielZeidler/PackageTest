6da5d3414bd1a5f88821194c7193b57cf9f79384
Daniel Zeidler <xs6929031@ix64016.inf.tu-dresden.de>
2022-08-04 10:48:40 +0200

    Add test packge content

---
+++ Runtime/GameUtilities.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ixlab.test
+{
+
+    public class GameUtilities : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
+
+
